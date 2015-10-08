5d6416182ccbf98316edd49f26f20c83efde99a8
Harold Shinsato <harold@shinsato.com>
2015-10-08 16:57:27 -0600

    a quick template

---
+++ things.js
@@ -0,0 +1,13 @@
+exports.Thing = function(name) {
+	// this is your exported constructor for 'Thing'
+	// add what you need here to make the tests pass.
+}
+
+exports.Food = function(name, foodPoints) {
+	// this is your exported constructor for 'Food'
+	// add what you need here to make the tests pass.
+};
+
+// remember to have the 'voodoo' code here to connect your prototype and constructor
+
+
